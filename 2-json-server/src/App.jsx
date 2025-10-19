a7661407e6823b77513793f26c5cacd195d40157
evasyukov <egor.vasyukov1@gmail.com>
2025-10-19 13:06:56 +0700

    - перенес разметку из предыдущего задания - изменил запрос данных

--- 2-json-server/src/App.jsx
+++ 2-json-server/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react"
+import "./App.css"
 
 function App() {
+  const [todos, setTodos] = useState([])
 
+  useEffect(() => {
+    fetch("http://localhost:3005/todos")
+      .then((response) => response.json())
+      .then((todoList) => {
+        setTodos(todoList)
+      })
+      .catch(() => console.log("Ошибка запроса"))
+  }, [])
+
+  console.log(todos)
 
   return (
-    <>
-
-    </>
+    <div className="todo-list">
+      {Object.entries(todos).map(({ id, title, completed }) => (
+        <div className="todo" key={id}>
+          <div className="todo__title"> {title}</div>
+          {completed ? (
+            <div className="todo_completed true">выполнено</div>
+          ) : (
+            <div className="todo_completed false"> не выполнено</div>
+          )}
+        </div>
+      ))}
+    </div>
   )
 }
 
